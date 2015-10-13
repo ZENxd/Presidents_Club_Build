2d1750b8e777e757fd02fa761d72e3db1b7f5607
Dave Stewart <dave@metanurb.com>
2015-10-13 14:44:53 -0400

    Review, action + Demo

--- scripts/scripts.7c6f9dc9.js
+++ scripts/scripts.7c6f9dc9.js
@@ -51,6 +51,16 @@ angular.module('presidentsClubApp', [
                 controller: 'ThanksCtrl',
                 controllerAs: 'thanks'
             })
+            .when('/list', {
+                templateUrl: 'views/list.html',
+                controller: 'ListCtrl',
+                controllerAs: 'list'
+            })
+            .when('/detail/:id', {
+                templateUrl: 'views/detail.html',
+                controller: 'DetailCtrl',
+                controllerAs: 'detail'
+            })
             .otherwise({
                 redirectTo: '/'
             });
@@ -240,9 +250,11 @@ angular.module('presidentsClubApp')
             modelService.getModel(function(result) {
                 $scope.nomineeModel = result;
                 //Bind user login credentials to niminee model -> nimonator attributes
+                /*
                 $scope.nomineeModel.nominator.nom_first = $rootScope.globals.currentUser.userFirst;
                 $scope.nomineeModel.nominator.nom_last = $rootScope.globals.currentUser.userLast;
                 $scope.nomineeModel.nominator.nom_email = $rootScope.globals.currentUser.userEmail;
+                */
             });
 
             //Top nav DOM visual switches
@@ -405,6 +417,91 @@ angular.module('presidentsClubApp')
             $scope.restart = function() {
             	modelService.resetModel();
                 $location.path('/nominee');
+            };
+
+        }
+    ]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name presidentsClubApp.controller:ListCtrl
+ * @description
+ * # ListCtrl
+ * Controller of the presidentsClubApp
+ */
+angular.module('presidentsClubApp')
+    .controller('ListCtrl', ['$scope', '$rootScope', '$location', 'demoService', 'settings', 
+        function($scope, $rootScope, $location, demoService, settings) {
+
+
+            settings.setValue('logo', true);
+            settings.setValue('back', false);
+            $scope.nomineesModel = null;
+
+            demoService.getNominees(function(result) {
+                $scope.nomineesModel = result;
+            });
+
+            $scope.detail = function(id) {
+                $location.path('/detail/' + id);
+            };
+
+        }
+    ]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name presidentsClubApp.controller:DetailCtrl
+ * @description
+ * # DetailCtrl
+ * Controller of the presidentsClubApp
+ */
+angular.module('presidentsClubApp')
+    .controller('DetailCtrl', ['$scope', '$rootScope', '$location', '$routeParams', 'demoService', 'settings', 
+        function($scope, $rootScope, $location, $routeParams, demoService, settings) {
+
+            settings.setValue('logo', false);
+            settings.setValue('back', true);
+
+            $scope.nomineeModel = null;
+            $scope.nomineeModelId = $routeParams.id;
+
+
+            demoService.queryNominee(function(result) {
+              $scope.nomineeModel = result;
+            },
+              //Pass id as param to service
+              $scope.nomineeModelId
+            );
+
+            $scope.approve = function(id) {
+                demoService.save(id, 'Approved');
+                $scope.back();
+            };
+
+            $scope.deny = function(id) {
+                demoService.save(id, 'Denied');
+                $scope.back();
+            };
+
+            $scope.back = function() {
+                $location.path('/list');
+            };
+
+            $scope.formatPhone = function(value) {
+                if (value && value.length === 10) {
+                    value = value.replace(/-/g, '');
+                    var areaCode = value.substring(0, 3);
+                    var exchange = value.substring(3, 6);
+                    var tail = value.substring(6);
+                    return "(" + areaCode + ")" + "-" + exchange + "-" + tail;
+                } else {
+                    return '';
+                }
             };
 
         }
@@ -688,12 +785,13 @@ angular.module('presidentsClubApp')
                     behavior: '',
                     leadership: ''
                 },
+                /*
                 nominator: {
                     first: '',
                     last: '',
                     email: '',
                     phone: ''
-                },
+                },*/
                 nominationStatus: ''
             };
             nomineeModel = angular.copy(template);
@@ -1383,4 +1481,141 @@ angular.module('presidentsClubApp')
             };
 
         });
+})();
+
+(function() {
+  'use strict';
+  angular.module('presidentsClubApp')
+    .service('demoService', function() {
+      
+      var data = [];
+      var template = {
+          number: '',
+          salesOrg: {},
+          region: {},
+          country: {},
+          salutation: {},
+          first: '',
+          last: '',
+          address: '',
+          officeTel: '',
+          mobileTel: '',
+          email: '',
+          title: {},
+          nominatedByManager: 'No',
+          recurringWinner: 'No',
+          winCount: {},
+          years: '',
+          performance: {
+              salesQuota: null,
+              sales: null,
+              percentOver: null,
+              percentLast: null
+          },
+          comments: {
+              performance: '',
+              planning: '',
+              relationship: '',
+              behavior: '',
+              leadership: ''
+          },
+          /*
+          nominator: {
+              first: '',
+              last: '',
+              email: '',
+              phone: ''
+          },*/
+          nominationStatus: ''
+      };
+
+      var nominees = [
+        {salutation: {id: 0, name: 'Mr.'},  number: '225087', first: 'BEN', last: 'EDWARDS', title:{id: 0, name: 'Accountant'}},
+        {salutation: {id: 3, name: 'Dr.'},  number: '155084', first: 'MATTHEW', last: ' TYRELL', title:{id: 1, name: 'Account Manager' }},
+        {salutation: {id: 2, name: 'Mrs.'}, number: '233087', first: 'ALLISON', last: 'CAMPBELL', title:{id: 2, name: 'District Sales Manager'}},
+        {salutation: {id: 0, name: 'Mr.'},  number: '234086', first: 'JESSE', last: 'BENNETT-CHAMBERLAIN', title:{id: 3, name: 'Product Designer'}},
+        {salutation: {id: 0, name: 'Mr.'},  number: '455087', first: 'KIRK', last: 'HERRON', title:{id: 4, name: 'Product Specialist'}},
+        {salutation: {id: 0, name: 'Mr.'},  number: '355087', first: 'TAYLOR', last: 'BOSWELL', title:{id: 0, name: 'Account Manager'}},
+        {salutation: {id: 1, name: 'Ms.'},  number: '255344', first: 'JENNIFER', last: 'BRADSHAW', title:{id: 1, name: 'Account Manager'}},
+        {salutation: {id: 0, name: 'Mr.'},  number: '255287', first: 'BRIAN', last: 'KAUFMAN', title:{id: 2, name: 'District Sales Managr'}},
+        {salutation: {id: 0, name: 'Mr.'},  number: '252587', first: 'CARLOS', last: 'SANCHEZ', title:{id: 3, name: 'Product Designer' }},
+        {salutation: {id: 0, name: 'Mr.'},  number: '285083', first: 'WEYLAND', last: 'ERICKSON', title:{id: 4, name: 'Product Specialist'}},
+        {salutation: {id: 1, name: 'Ms.'},  number: '143087', first: 'AMY', last: 'PARKER', title:{id: 0, name: 'Accountant'}},
+        {salutation: {id: 3, name: 'Dr.'},  number: '255025', first: 'DUSTIN', last: 'ANDERSON', title:{id: 1, name: 'Account Manager'}}
+      ];
+
+      var details = {
+        so: {id: '0', name: 'LSAG/ACG'},
+        region: {id: '0', name: 'China'}, 
+        country: {id: '0', name: 'USA'},
+        address: '5301 Stevens Creek Blvd., Santa Clara CA, 95051',
+        officeTel: '8774244536', mobileTel: '4083458886',
+        recurringWinner: 'Yes',
+        performance: {salesQuota: 1500000, sales: 1700000, percentOver: 110, percentLast: 103},
+        nominatedByManager: 'Yes',
+        winCount: {id: '1', name: '2'},
+        years: '2010, 2011'
+      };
+
+      var comment = 'Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies' +
+      'vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit' +
+      'amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor' +
+      'fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.';
+      
+      var nominators = [
+        {first: 'Trent', last: 'Walton', email: '', phone: '8774244536'}, {first: 'Jeremy', last: 'Turner', email: '', phone: '8774244536'}, {first: 'Megham', last: 'Armstrong', email: '', phone: '8774244536'}, {first: 'Ed', last: 'Williams', email: '', phone: '8774244536'},
+        {first: 'Trent', last: 'Walton', email: '', phone: '8774244536'}, {first: 'Jeremy', last: 'Turner', email: '', phone: '8774244536'}, {first: 'Megham', last: 'Armstrong', email: '', phone: '8774244536'}, {first: 'Ed', last: 'Williams', email: '', phone: '8774244536'},
+        {first: 'Trent', last: 'Walton', email: '', phone: '8774244536'}, {first: 'Jeremy', last: 'Turner', email: '', phone: '8774244536'}, {first: 'Megham', last: 'Armstrong', email: '', phone: '8774244536'}, {first: 'Ed', last: 'Williams', email: '', phone: '8774244536'}
+      ];
+
+      this.makeNominees = function(){
+        angular.forEach(nominees, function(nominee, index){
+          var employee = angular.copy(template);
+          employee.id = index;
+          employee.number = nominees[index].number;
+          employee.salutation = nominees[index].salutation;
+          employee.first = nominees[index].first;
+          employee.last = nominees[index].last;
+          employee.title = nominees[index].title;
+          employee.email = nominees[index].first+'@agilent.com';
+          employee.so = details.so;
+          employee.region = details.region;
+          employee.country = details.country;
+          employee.address = details.address;
+          employee.officeTel = details.officeTel;
+          employee.mobileTel = details.mobileTel;
+          employee.recurringWinner = details.recurringWinner;
+          employee.nominatedByManager = details.nominatedByManager;
+          employee.winCount = details.winCount;
+          employee.years = details.years;
+          employee.performance.salesQuota = details.performance.salesQuota;
+          employee.performance.sales = details.performance.sales;
+          employee.performance.percentOver = details.performance.percentOver;
+          employee.performance.percentLast = details.performance.percentLast;
+          employee.comments.performance = comment;
+          employee.comments.planning = comment;
+          employee.comments.relationship = comment;
+          employee.comments.behavior = comment;
+          employee.comments.leadership = comment;
+          employee.nomStatus = 'Awaiting Approval';
+          employee.nominator = {first: nominators[index].first, last: nominators[index].last, 
+                                email: nominators[index].first+'@agilent.com',
+                                phone: nominators[index].phone};
+          data.push(employee);
+        });
+      };
+      this.makeNominees();
+
+      this.getNominees = function(callback) {
+        callback(data);
+      };
+
+      this.queryNominee = function(callback, id) {
+        callback(data[id]);
+      };
+
+      this.save = function(id, value){
+        data[id].nomStatus = value;
+      };
+    });
 })();
